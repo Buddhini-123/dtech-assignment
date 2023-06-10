17486f351c28ebe18312dce5878cc5020be38192
Buddhini Jayakody <buddhini.jayakody@elegantmedia.lk>
2023-06-10 13:33:35 +0530

    logout route fixed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -83,7 +83,7 @@
                 <ul class="navbar-nav ml-auto">
                     <!-- Nav Item - User Information -->
                     <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="{{'logout'}}" id="userDropdown" role="button"
+                        <a class="nav-link dropdown-toggle" href="/logout" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">Log out</span>
                         </a>
